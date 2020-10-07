387ea7b9f886d4fb6327f314fdea4c5d26d1a75b
r0nunes <r0nunes@users.noreply.github.com>
2020-10-07 01:25:58 +0000

    Deploying to gh-pages from  @ e2bc8ad8b5465fd3094d8c9aa00ba45395fad28b 🚀

---
+++ reporter.js
@@ -0,0 +1,20 @@
+const reporter = require('cucumber-html-reporter')
+
+const options = {
+    theme: 'bootstrap',
+    jsonDir: 'cypress/reports/cucumber-json',
+    output: 'cypress/reports/index.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true,
+    metadata: {
+        "App Name":"Demo Automation",
+        "Test Environment": "STAGING",
+        "Browser": "Electron",
+        "Platform": "Ubuntu",
+        "Parallel": "Scenarios",
+        "Executed": "Remote"
+    }
+};
+
+reporter.generate(options)
